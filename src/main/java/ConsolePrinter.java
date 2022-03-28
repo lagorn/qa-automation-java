6f2c5cf8e098d5aca194515bae3312d28d1ead1c
a.v.altukhov <a.v.altukhov@tinkoff.ru>
2022-03-28 20:54:43 +0300

    feat: created documentation for class ConsolePrinter

--- src/main/java/ConsolePrinter.java
+++ src/main/java/ConsolePrinter.java
@@ -1,4 +1,13 @@
+/**
+ *  Класс ConsolePrinter печатает данные в консоль.
+ *  @autor Алтухов Александр
+ *
+ */
 public class ConsolePrinter {
+    /**
+     * Функция вывода сообщения в консоль {@link ConsolePrinter}
+     * @param message - сообщение (String)
+     */
     public static void print(String message) {
         System.out.println(message);
     }
