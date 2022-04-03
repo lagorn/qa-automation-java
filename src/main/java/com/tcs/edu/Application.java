7f513c43350971f8bd0ccfef760d98e06be0c6c5
a.v.altukhov <a.v.altukhov@tinkoff.ru>
2022-04-03 18:13:44 +0300

    feat: add variable global messageCount

--- src/main/java/com/tcs/edu/Application.java
+++ src/main/java/com/tcs/edu/Application.java
@@ -3,9 +3,10 @@ package com.tcs.edu;
 import com.tcs.edu.printer.ConsolePrinter;
 
 import static com.tcs.edu.decorator.TimestampMessageDecorator.decorate;
+import static com.tcs.edu.decorator.TimestampMessageDecorator.messageCount;
 
 class Application {
     public static void main(String[] args) {
-        System.out.println(decorate(" Hello world!"));
+        System.out.println(messageCount + " " + decorate(" Hello world!"));
     }
 }
--- src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
+++ src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
@@ -4,9 +4,11 @@ import java.time.Instant;
 
 public class TimestampMessageDecorator {
     private static String decoratedMessage;
+    public static int messageCount = 0;
 
     public static String decorate(String message){
         decoratedMessage = Instant.now() + message;
+        messageCount++;
         return decoratedMessage;
     }
 }
