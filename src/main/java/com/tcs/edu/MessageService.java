ee117dbeb37002580557fc46b3a4e95349d39639
a.v.altukhov <a.v.altukhov@tinkoff.ru>
2022-04-21 21:03:24 +0300

    feat: add check for null

--- src/main/java/com/tcs/edu/MessageService.java
+++ src/main/java/com/tcs/edu/MessageService.java
@@ -3,17 +3,24 @@ package com.tcs.edu;
 
 import com.tcs.edu.decorator.Severity;
 
+import java.util.Objects;
+
 import static com.tcs.edu.decorator.SeverityDecorator.addLevelDecorator;
 import static com.tcs.edu.decorator.TimestampMessageDecorator.decorate;
 import static com.tcs.edu.printer.ConsolePrinter.print;
 
 public class MessageService {
-    public static void gluingPrint(Severity severity,String message, String... messages){
+    public static void gluingPrint(Severity severity, String message, String... messages) {
 
-        print(decorate(message + addLevelDecorator(severity)));
+        if ((message != null) && (Objects.nonNull(severity))) {
+            print(decorate(message + addLevelDecorator(severity)));
+        }
 
-        for (String mes: messages) {
-            print(decorate(mes + addLevelDecorator(severity)));
+        for (String mes : messages) {
+
+            if ((mes != null) && (Objects.nonNull(severity))) {
+                print(decorate(mes + addLevelDecorator(severity)));
+            }
         }
     }
 }
