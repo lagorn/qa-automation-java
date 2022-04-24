1440f5fa1e730ee1a70243d957b0a60e575b8a82
a.v.altukhov <a.v.altukhov@tinkoff.ru>
2022-04-24 22:14:55 +0300

    feat: add #07-3

--- src/main/java/com/tcs/edu/Application.java
+++ src/main/java/com/tcs/edu/Application.java
@@ -1,6 +1,9 @@
 package com.tcs.edu;
 
+import com.tcs.edu.decorator.Doubling;
+
 import static com.tcs.edu.MessageService.gluingPrint;
+import static com.tcs.edu.decorator.Doubling.*;
 import static com.tcs.edu.decorator.MessageOrder.*;
 import static com.tcs.edu.decorator.Severity.*;
 
@@ -8,6 +11,6 @@ class Application {
     public static void main(String[] args) {
         gluingPrint(MAJOR, "Hello world!", "Hello Piter!", "Hello John!");
         gluingPrint(MAJOR, ASR, "Hello world!", "Hello Mum!", "Hello Dad!", "Hello Mum!", "Hello Dad!", "Hello Michael!");
-        gluingPrint(MAJOR, DESC, "Hello world!", "Hello Noah!", "Hello Liam!", "Hello Jacob!", "Hello William!");
+        gluingPrint(MAJOR, DISTINCT, DESC, "Hello world!", "Hello world!", "Hello Liam!", "Hello Jacob!", "Hello William!");
     }
 }
--- src/main/java/com/tcs/edu/MessageService.java
+++ src/main/java/com/tcs/edu/MessageService.java
@@ -1,6 +1,7 @@
 package com.tcs.edu;
 
 
+import com.tcs.edu.decorator.Doubling;
 import com.tcs.edu.decorator.MessageOrder;
 import com.tcs.edu.decorator.Severity;
 
@@ -49,5 +50,55 @@ public class MessageService {
                 }
             }
         }
+    }
+
+    public static void gluingPrint(Severity severity, Doubling doubling, MessageOrder messageOrder, String message, String... messages) {
+        if (doubling == Doubling.DOUBLES) {
+            gluingPrint(severity, messageOrder, message, messages);
+        } else {
+
+            String[] arrayMessage = new String[messages.length + 1];
+
+            if ((message != null) && (Objects.nonNull(severity)) && (messageOrder != null)) {
+                if (messageOrder == MessageOrder.ASR) {
+                    messageCount = 1;
+                    print(decorate(message + " " + messageCount++ + addLevelDecorator(severity)));
+                    arrayMessage[0] = message;
+
+                    for (int i = 0; i < messages.length; i++, messageCount++) {
+                        if (messages[i] != null) {
+                            for (int j = 1; j < arrayMessage.length; j++) {
+                                if (!duplicateSearch(messages[i], arrayMessage)) {
+                                    print(decorate(messages[i] + " " + messageCount + addLevelDecorator(severity)));
+                                    arrayMessage[j] = messages[i];
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    messageCount = messages.length + 1;
+                    print(decorate(message + " " + messageCount-- + addLevelDecorator(severity)));
+                    arrayMessage[0] = message;
+
+                    for (int i = messages.length - 1; i >= 0; i--, messageCount--) {
+                        for (int j = 1; j < arrayMessage.length; j++) {
+                            if (!duplicateSearch(messages[i], arrayMessage)) {
+                                print(decorate(messages[i] + " " + messageCount + addLevelDecorator(severity)));
+                                arrayMessage[j] = messages[i];
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private static boolean duplicateSearch(String message, String... messages) {
+        for (String checkedMessage : messages) {
+            if (Objects.equals(message, checkedMessage)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
